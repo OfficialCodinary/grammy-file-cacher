7eb403a83844ae639cc175400e4d3b7f3697671a
Codinary <133005277+OfficialCodinary@users.noreply.github.com>
2024-02-22 06:58:51 +0530

    Rewrite in TS and add concurrency 50%

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 const { InputFile } = require('grammy')
 const map = new Map()
+const concurreny = new Map()
 
 const CacheMedias = async (prev, method, payload, signal) => {
     let response;
@@ -7,12 +8,25 @@ const CacheMedias = async (prev, method, payload, signal) => {
     if (method === 'sendPhoto' || method === 'sendAudio' || method === 'sendVideo' || method === 'sendAnimation' || method === 'sendDocument' || method === 'sendVideoNote' || method === 'sendVoice') {
 
         let media = mediaTypes.find(type => type in payload);
+        if (!media) return prev(method, payload, signal)
         const mediaString = payload[media] instanceof InputFile ? payload[media].fileData : payload[media]
 
         if (map.has(mediaString)) {
             payload[media] = map.get(mediaString)
             response = prev(method, payload, signal)
         } else {
+            if (concurreny.has(mediaString)) {
+                await new Promise<void>((resolve) => {
+                    const interval = setInterval(() => {
+                        if (!concurreny.has(mediaString)) {
+                            clearInterval(interval);
+                            resolve();
+                        }
+                    }, 50);
+                });
+            } else {
+                concurreny.set(mediaString, true)
+            }
             response = prev(method, payload, signal)
 
             if (media === 'photo') {
@@ -20,9 +34,10 @@ const CacheMedias = async (prev, method, payload, signal) => {
             } else {
                 map.set(mediaString, (await response).result[media].file_id)
             }
+            concurreny.delete(mediaString)
         }
     } else if (method === 'sendMediaGroup' || method === 'editMessageMedia') {
-        const tempMedias = []
+        const tempMedias: string | null[] = []
 
         for (const i in payload.media) {
             const media = payload.media[i]
@@ -30,7 +45,9 @@ const CacheMedias = async (prev, method, payload, signal) => {
             if (map.has(mediaString)) {
                 payload.media[i].media = map.get(mediaString)
                 tempMedias.push(null);
-            } else tempMedias.push(mediaString)
+            } else {
+                tempMedias.push(mediaString)
+            }
 
         }
         response = prev(method, payload, signal)
@@ -38,6 +55,7 @@ const CacheMedias = async (prev, method, payload, signal) => {
         const resp = (await response).result
         if (!resp.length) {
             let type = mediaTypes.find(type => type in resp);
+            if (!type) return response
             if (tempMedias[0] !== null) {
                 let mediaString = tempMedias[0]
                 if (type === 'photo') {
@@ -50,6 +68,7 @@ const CacheMedias = async (prev, method, payload, signal) => {
             for (const i in resp) {
                 const media = resp[i] || resp
                 let type = mediaTypes.find(type => type in media);
+                if (!type) return response
                 if (tempMedias[i] !== null) {
                     let mediaString = tempMedias[i]
                     if (type === 'photo') {
@@ -67,6 +86,4 @@ const CacheMedias = async (prev, method, payload, signal) => {
     return response
 }
 
-module.exports = {
-    CacheMedias
-}
+export { CacheMedias }

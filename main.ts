0be3a199bdbea5e44ab93bc28102c4706663ac7d
Codinary <133005277+OfficialCodinary@users.noreply.github.com>
2024-02-22 09:59:46 +0530

    Now uses awaitable arrays.
    Thanks to @Knightniwrem

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 const { InputFile } = require('grammy')
-const map = new Map()
-const parallel = new Map()
+
+type MaybePromise<T> = Promise<T> | T;
+
+const map = new Map<string, MaybePromise<string>>()
+
+function getFileID(response, media: string): Promise<string> {
+    return new Promise(async (resolve) => {
+        const result = await response;
+        let fileId = result.result[media][media === 'photo' ? 0 : ''].file_id
+        resolve(fileId);
+    })
+}
 
 const CacheMedias = () => async (prev, method, payload, signal) => {
     let response;
@@ -12,29 +22,14 @@ const CacheMedias = () => async (prev, method, payload, signal) => {
         const mediaString = payload[media] instanceof InputFile ? payload[media].fileData : payload[media]
 
         if (map.has(mediaString)) {
-            payload[media] = map.get(mediaString)
+            console.log(await map.get(mediaString))
+            payload[media] = await map.get(mediaString)
             response = prev(method, payload, signal)
         } else {
-            if (parallel.has(mediaString)) {
-                await new Promise<void>((resolve) => {
-                    const interval = setInterval(() => {
-                        if (!parallel.has(mediaString)) {
-                            clearInterval(interval);
-                            resolve();
-                        }
-                    }, 50);
-                });
-            } else {
-                parallel.set(mediaString, true)
-            }
             response = prev(method, payload, signal)
 
-            if (media === 'photo') {
-                map.set(mediaString, (await response).result.photo[0].file_id)
-            } else {
-                map.set(mediaString, (await response).result[media].file_id)
-            }
-            parallel.delete(mediaString)
+            map.set(mediaString, getFileID(response, media))
+
         }
     } else if (method === 'sendMediaGroup' || method === 'editMessageMedia') {
         const tempMedias: string | null[] = []
@@ -52,17 +47,13 @@ const CacheMedias = () => async (prev, method, payload, signal) => {
         }
         response = prev(method, payload, signal)
 
-        const resp = (await response).result
+        const resp = response
         if (!resp.length) {
             let type = mediaTypes.find(type => type in resp);
             if (!type) return response
             if (tempMedias[0] !== null) {
                 let mediaString = tempMedias[0]
-                if (type === 'photo') {
-                    map.set(mediaString, resp.photo[0].file_id)
-                } else {
-                    map.set(mediaString, resp[type].file_id)
-                }
+                map.set(mediaString, getFileID(resp, type))
             }
         } else {
             for (const i in resp) {
@@ -71,11 +62,7 @@ const CacheMedias = () => async (prev, method, payload, signal) => {
                 if (!type) return response
                 if (tempMedias[i] !== null) {
                     let mediaString = tempMedias[i]
-                    if (type === 'photo') {
-                        map.set(mediaString, media.photo[0].file_id)
-                    } else {
-                        map.set(mediaString, media[type].file_id)
-                    }
+                    map.set(mediaString, getFileID(media, type))
                 }
             }
         }
